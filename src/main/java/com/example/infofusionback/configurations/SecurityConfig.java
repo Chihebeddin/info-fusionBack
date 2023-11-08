14cf417003498b6c2078678be69f244425a67078
chihebeddin <chihebeddinhamrouni@gmail.com>
2023-11-08 16:32:54 +0100

    modification de la base de données

--- src/main/java/com/example/infofusionback/configurations/SecurityConfig.java
+++ src/main/java/com/example/infofusionback/configurations/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/api/test/**").permitAll()
+                                .requestMatchers("/clients/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
